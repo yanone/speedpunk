d5b9c45f8654c64a237e3d898097cc8f2c0de662
Yanone <post@yanone.de>
2019-07-30 11:37:12 +0200

    Improved welcome string

--- Glyphs/SpeedPunk.glyphsReporter/Contents/Resources/speedpunk/speedpunklib.py
+++ Glyphs/SpeedPunk.glyphsReporter/Contents/Resources/speedpunk/speedpunklib.py
@@ -271,8 +271,8 @@ class SpeedPunkLib(object):
 			if not Glyphs.defaults[self.trialKey]:
 				Glyphs.defaults[self.trialKey] = int(time.time())
 				Message(Glyphs.localize({'en': u'Welcome to Speed Punk %s' % VERSION, 
-				   'en': u'Willkommen zu Speed Punk %s' % VERSION}), Glyphs.localize({'en': "Your %s-day trial period of Speed Punk has started. You\xe2\x80\x99ll find me in the View menu under ‘Show Speed Punk’ or with the keyboard shortcut Cmd+Shift+X.\nTo learn more about Speed Punk, please visit\nhttps://yanone.de/software/speedpunk/.\n\nEnjoy and make sure to follow @yanone on Twitter." % self.trialPeriod, 
-				   'de': u"Deine %s-Tage-Testperiode von Speed Punk hat begonnen. Du findest mich im Ansicht-Men\xfc unter ‘Speed Punk anzeigen’ oder mit dem Tastenk\xfcrzel Cmd+Shift+X.\nMehr \xfcber Speed Punk erf\xe4hrst Du hier:\nhttps://yanone.de/software/speedpunk/.\n\nViel Spaß und wir sehen uns bei @yanone auf Twitter." % self.trialPeriod}))
+				   'en': u'Willkommen zu Speed Punk %s' % VERSION}), Glyphs.localize({'en': "Your %s-day trial period of Speed Punk has started. You\xe2\x80\x99ll find me in the View menu under ‘Show Speed Punk’ or with the keyboard shortcut Cmd+Shift+X. The plugin settings have moved into the context menu (right click).\nTo learn more about Speed Punk, please visit\nhttps://yanone.de/software/speedpunk/.\n\nEnjoy and make sure to follow @yanone on Twitter." % self.trialPeriod, 
+				   'de': u"Deine %s-Tage-Testperiode von Speed Punk hat begonnen. Du findest mich im Ansicht-Men\xfc unter ‘Speed Punk anzeigen’ oder mit dem Tastenk\xfcrzel Cmd+Shift+X. Die Plugin-Einstellungen sind ins Kontextmenü (Rechtsklick) gewandert.\nMehr \xfcber Speed Punk erf\xe4hrst Du hier:\nhttps://yanone.de/software/speedpunk/.\n\nViel Spaß und wir sehen uns bei @yanone auf Twitter." % self.trialPeriod}))
 			if 0 < self.trialDaysLeft() <= self.trialPeriod:
 				Glyphs.showNotification(Glyphs.localize({'en': 'Speed Punk trial period', 
 				   'en': u'Speed Punk Testperiode'}), Glyphs.localize({'en': '%s days left' % self.trialDaysLeft(), 
@@ -286,8 +286,8 @@ class SpeedPunkLib(object):
 				'en': u'Welcome to Speed Punk %s' % VERSION,
 				'de': u'Willkommen zu Speed Punk %s' % VERSION,
 			}), Glyphs.localize({
-				'en': u'Thank you for choosing Speed Punk. You’ll find me in the View menu under ‘Show Speed Punk’ or with the keyboard shortcut Cmd+Shift+X.\n\nEnjoy and make sure to follow @yanone on Twitter.',
-				'de': u'Danke zur Wahl von Speed Punk. Du findest mich im Ansicht-Menü unter ‘Speed Punk anzeigen’ oder mit dem Tastenkürzel Cmd+Shift+X.\n\nViel Spaß und wir sehen uns bei @yanone auf Twitter.',
+				'en': u'Thank you for choosing Speed Punk. You’ll find me in the View menu under ‘Show Speed Punk’ or with the keyboard shortcut Cmd+Shift+X. The plugin settings have moved into the context menu (right click).\n\nEnjoy and make sure to follow @yanone on Twitter.',
+				'de': u'Danke zur Wahl von Speed Punk. Du findest mich im Ansicht-Menü unter ‘Speed Punk anzeigen’ oder mit dem Tastenkürzel Cmd+Shift+X. Die Plugin-Einstellungen sind ins Kontextmenü (Rechtsklick) gewandert.\n\nViel Spaß und wir sehen uns bei @yanone auf Twitter.',
 			})
 			)
 		return
